c97a3432f1dcc2f0308e0dc4cf239ca98d65f544
Ben Edgington <ben@benjaminion.xyz>
2021-03-07 07:31:21 +0000

    Add a version that uses the integers mod 17

--- README.md
+++ README.md
@@ -1,23 +1,26 @@
 # Toy example of data recovery
 
-The code implements, for didactic purposes, a very simple example of data recovery. It encodes a tiny case of Vitalik Buterin's method described in [this article](https://ethresear.ch/t/reed-solomon-erasure-code-recovery-in-n-log-2-n-time-with-ffts/3039?u=benjaminion).
+The code implements, for didactic purposes, a very simple example of data recovery. It exemplifies a tiny case of Vitalik Buterin's method described in [this article](https://ethresear.ch/t/reed-solomon-erasure-code-recovery-in-n-log-2-n-time-with-ffts/3039?u=benjaminion).
 
-See the accompanying write-up [here](https://hackmd.io/@benjaminion/data_recovery).
+See the accompanying write-up [here](https://hackmd.io/@benjaminion/data_recovery). For something more capable, see my [`c-kzg` repo](https://github.com/benjaminion/c-kzg).
 
-For something more capable, see my [`c-kzg` repo](https://github.com/benjaminion/c-kzg).
+There are two versions:
+
+1. A version that encodes the data and does the calculations over the complex integers. This uses _i_ as the primitive fourth root of unity, giving the roots for the fourier transforms as [1, i, -1, -i]. Find it in _complex.c_.
+2. A version that encodes the data and does the calculations over the integers mod 17, a finite field. This uses 4 as the primitive fourth root of unity, giving the roots for the fourier transforms as [1, 4, 16, 13]. Find it in _finite.c_.
 
 ## Compile
 
 Either
 
 ```
-gcc main.c
+cc complex.c
 ```
 
 or
 
 ```
-clang main.c
+cc finite.c
 ```
 
 Or choose your favourite C compiler.
@@ -29,6 +32,10 @@ Or choose your favourite C compiler.
 ```
 
 ## Output
+
+Note that, if you substitue `i = 4` in the output from _complex.c_, and make everything positive modulo 17, you end up with exactly the same output as from _finite.c_.
+
+From _complex.c_
 
 ```
     Initial values: [5, 7, 0, 0]
@@ -45,3 +52,21 @@ Or choose your favourite C compiler.
   Scaled recovered: [5, 14, 0, 0]
   Recovered values: [5, 7, 0, 0]
 ```
+
+From _finite.c_
+
+```
+    Initial values: [5, 7, 0, 0]
+      Data encoded: [12, 16, 15, 11]
+ Data with missing: [12, 0, 0, 11]
+     ZeroPoly eval: [11, 0, 0, 7]
+           EZ eval: [13, 0, 0, 9]
+      EZ = DZ poly: [14, 8, 1, 7]
+    DZ poly scaled: [14, 16, 4, 5]
+   ZeroPoly scaled: [13, 11, 4, 0]
+    DZ eval scaled: [5, 3, 14, 0]
+  Zero eval scaled: [11, 2, 6, 16]
+     Quotient eval: [2, 10, 8, 0]
+  Scaled recovered: [5, 14, 0, 0]
+  Recovered values: [5, 7, 0, 0]
+  ```
--- finite.c
+++ finite.c
@@ -20,9 +20,12 @@
  *
  * It is intended to be read alongside the accompanying writeup at https://hackmd.io/@benjaminion/data_recovery
  *
+ * This does the data encoding over a finite field (integers mod 17), using 4 as the primitive fourth root of 1. This
+ * gives the powers of roots of unity we use as [1, 4, 16, 13].
+ *
  * Notes:
  *  - It is hardcoded to deal with only 4 samples (two data elements!), up to two of which can be lost.
- *  - This is not implemented over a finite field as per Eth2, but over the complex integers.
+ *  - The division is hardcoded for MOD = 17 because I am lazy.
  *
  * For a proper implementation, see my library https://github.com/benjaminion/c-kzg
  */
@@ -31,89 +34,49 @@
 #include <stdbool.h>
 #include <assert.h>
 
-typedef int number;
+#define MOD 17
 
-typedef struct {
-    number re;
-    number im;
-} complex;
+// Fourth root of 1
+const int i = 4;
 
-const complex zero = {0, 0};
-const complex i = {0, 1};
-const complex two = {2, 0};
-const complex four = {4, 0};
-
-void print_c(complex a) {
-    printf("%d", a.re);
-    if (a.im != 0) {
-        if (a.im > 0) {
-            if (a.im == 1)
-                printf(" + i");
-            else
-                printf(" + %di", a.im);
-        } else {
-            if (a.im == -1)
-                printf(" - i");
-            else
-                printf(" - %di", -a.im);
-        }
-    }
+void print_poly(const char *s, const int *a) {
+    printf("%18s: [%d, %d, %d, %d]\n", s, a[0], a[1], a[2], a[3]);
 }
 
-void print_poly(const char *s, const complex *a) {
-    printf("%18s: [", s);
-    print_c(a[0]);
-    printf(", ");
-    print_c(a[1]);
-    printf(", ");
-    print_c(a[2]);
-    printf(", ");
-    print_c(a[3]);
-    printf("]\n");
+bool equal(int a, int b) {
+    return a % MOD == b % MOD;
 }
 
-bool equal(complex a, complex b) {
-    return a.re == b.re && a.im == b.im;
+int neg(int a) {
+    return (MOD - a);
 }
 
-complex new_complex(number re, number im) {
-    complex ret = {re, im};
-    return ret;
+int add(int a, int b) {
+    return (a + b) % MOD;
 }
 
-complex cconj(complex a) {
-    return new_complex(a.re, -a.im);
+int sub(int a, int b) {
+    return (a + neg(b)) % MOD;
 }
 
-complex add(complex a, complex b) {
-    return new_complex(a.re + b.re, a.im + b.im);
+int mul(int a, int b) {
+    return (a * b) % MOD;
 }
 
-complex sub(complex a, complex b) {
-    return new_complex(a.re - b.re, a.im - b.im);
-}
-
-complex mul(complex a, complex b) {
-    return new_complex(a.re * b.re - a.im * b.im, a.re * b.im + a.im * b.re);
-}
-
-complex div(complex a, complex b) {
-    complex b_conj = cconj(b);
-    complex ab_conj = mul(a, b_conj);
-    complex bb_conj = mul(b, b_conj);
-    complex ret = new_complex(ab_conj.re / bb_conj.re, ab_conj.im / bb_conj.re);
-    // Can fail due to the integer division, or division by zero:
+int div(int a, int b) {
+    int inv[] = {0, 1, 9, 6, 13, 7, 3, 5, 15, 2, 12, 14, 10, 4, 11, 8, 16};
+    int ret = (a * inv[b % MOD]) % MOD;
     assert(equal(a, mul(ret, b)));
     return ret;
 }
 
 // Forward Fourier Transform: converts polynomial coefficients into polynomial evaluations at roots of unity
 // The roots of unity we are using are [1, i, -1, -i] in that order.
-void eval_from_poly(complex *eval, const complex *coeffs) {
-    complex c0_p_c2 = add(coeffs[0], coeffs[2]);
-    complex c0_m_c2 = sub(coeffs[0], coeffs[2]);
-    complex c1_p_c3 = add(coeffs[1], coeffs[3]);
-    complex c1_m_c3 = sub(coeffs[1], coeffs[3]);
+void eval_from_poly(int *eval, const int *coeffs) {
+    int c0_p_c2 = add(coeffs[0], coeffs[2]);
+    int c0_m_c2 = sub(coeffs[0], coeffs[2]);
+    int c1_p_c3 = add(coeffs[1], coeffs[3]);
+    int c1_m_c3 = sub(coeffs[1], coeffs[3]);
     eval[0] = add(c0_p_c2, c1_p_c3);
     eval[1] = add(c0_m_c2, mul(i, c1_m_c3));
     eval[2] = sub(c0_p_c2, c1_p_c3);
@@ -122,22 +85,22 @@ void eval_from_poly(complex *eval, const complex *coeffs) {
 
 // Reverse Fourier Transform: converts polynomial evaluations at roots of unity into polynomial coefficients
 // The roots of unity we are using are [1, i, -1, -i] in that order.
-void poly_from_eval(complex *coeffs, const complex *eval) {
-    complex c0_p_c2 = add(eval[0], eval[2]);
-    complex c0_m_c2 = sub(eval[0], eval[2]);
-    complex c1_p_c3 = add(eval[1], eval[3]);
-    complex c1_m_c3 = sub(eval[1], eval[3]);
+void poly_from_eval(int *coeffs, const int *eval) {
+    int c0_p_c2 = add(eval[0], eval[2]);
+    int c0_m_c2 = sub(eval[0], eval[2]);
+    int c1_p_c3 = add(eval[1], eval[3]);
+    int c1_m_c3 = sub(eval[1], eval[3]);
     coeffs[0] = add(c0_p_c2, c1_p_c3);
     coeffs[1] = sub(c0_m_c2, mul(i, c1_m_c3));
     coeffs[2] = sub(c0_p_c2, c1_p_c3);
     coeffs[3] = add(c0_m_c2, mul(i, c1_m_c3));
-    for (int j = 0; j < 4; j++) coeffs[j] = div(coeffs[j], four);
+    for (int j = 0; j < 4; j++) coeffs[j] = div(coeffs[j], 4);
 }
 
 // Given polynomial p(x), return the polynomial p(x / k).
 // Does this by multiplying the coefficients by powers of k.
-void scale(complex *a, complex k) {
-    complex fac = k;
+void scale(int *a, int k) {
+    int fac = k;
     a[1] = mul(a[1], fac);
     fac = mul(fac, k);
     a[2] = mul(a[2], fac);
@@ -147,8 +110,8 @@ void scale(complex *a, complex k) {
 
 // Given polynomial p(x), return the polynomial p(x * k).
 // Does this by dividing the coefficients by powers of k.
-void unscale(complex *a, complex k) {
-    complex fac = k;
+void unscale(int *a, int k) {
+    int fac = k;
     a[1] = div(a[1], fac);
     fac = mul(fac, k);
     a[2] = div(a[2], fac);
@@ -159,11 +122,11 @@ void unscale(complex *a, complex k) {
 int main() {
     // My data is [5, 7] - treat it as the the coefficients of a polynomial, D(x).
     // The data is two elements, and then extended with the same number of zeros.
-    const complex data_poly[] = {{5, 0}, {7, 0}, {0, 0}, {0, 0}};
+    const int data_poly[] = {5, 7, 0, 0};
     print_poly("Initial values", data_poly);
 
     // Encode my data by evaluating the polynomial at the roots of unity.
-    complex data_eval[4];
+    int data_eval[4];
     eval_from_poly(data_eval, data_poly);
     print_poly("Data encoded", data_eval);
 
@@ -171,19 +134,19 @@ int main() {
     // -  position 1, corresponding to r^1 = i
     // -  position 2, correspoding to r^2 = -1
     // But could be any two elements. r is our fourth-root of unity, i.
-    data_eval[1] = new_complex(0, 0);
-    data_eval[2] = new_complex(0, 0);
+    data_eval[1] = 0;
+    data_eval[2] = 0;
     print_poly("Data with missing", data_eval);
 
     // Construct the zero polynomial as the product of (1 - r^j) for each of the indices j that is missing.
-    // Our zero poly is (x - r^1)(x - r^2) = (x - i)(x + 1) = x^2 + (1 - i)x - i
-    complex zero_poly[] = {{0, -1}, {1, -1}, {1, 0}, {0, 0}};
-    complex zero_poly_eval[4];
+    // Our zero poly is (x - r^1)(x - r^2) = (x - 4)(x - 16) = x^2 - 3x + 13 = x^2 + 14x + 13
+    int zero_poly[] = {13, 14, 1, 0};
+    int zero_poly_eval[4];
     eval_from_poly(zero_poly_eval, zero_poly);
     print_poly("ZeroPoly eval", zero_poly_eval);
 
     // Create the evaluation (E * Z)(r^j) for each j. It has zeros at indices where we lost data.
-    complex ez_eval[4];
+    int ez_eval[4];
     for (int j = 0; j < 4; j++) {
         ez_eval[j] = mul(data_eval[j], zero_poly_eval[j]);
     }
@@ -191,14 +154,14 @@ int main() {
 
     // Interpolate to get (E * Z)(x) = (D * Z)(x). The equality holds since, by construction, E and D agree both where
     // non-zero, and where zero due to Z.
-    complex dz_poly[4];
+    int dz_poly[4];
     poly_from_eval(dz_poly, ez_eval);
     print_poly("EZ = DZ poly", dz_poly);
 
     // Scale ("shift") the polynomials so that we can divide them without hitting a zero in the zero poly.
     // Any scale factor is ok, as long as it is not one of the roots of unity (or zero).
-    scale(dz_poly, two);
-    scale(zero_poly, two);
+    scale(dz_poly, 2);
+    scale(zero_poly, 2);
     print_poly("DZ poly scaled", dz_poly);
     print_poly("ZeroPoly scaled", zero_poly);
 
@@ -207,27 +170,27 @@ int main() {
     // back to polynomial form.
 
     // Convert (D * Z)(x / 2) and Z(x / 2) to evaluation form,
-    complex dz_scaled_eval[4];
-    complex zero_scaled_eval[4];
+    int dz_scaled_eval[4];
+    int zero_scaled_eval[4];
     eval_from_poly(dz_scaled_eval, dz_poly);
     eval_from_poly(zero_scaled_eval, zero_poly);
     print_poly("DZ eval scaled", dz_scaled_eval);
     print_poly("Zero eval scaled", zero_scaled_eval);
 
     // Divide pointwise
-    complex quotient_eval[4];
+    int quotient_eval[4];
     for (int j = 0; j < 4; j++) {
         quotient_eval[j] = div(dz_scaled_eval[j], zero_scaled_eval[j]);
     }
     print_poly("Quotient eval", quotient_eval);
 
     // Convert back to polynomial form to get D(x / 2).
-    complex recovered_poly[4];
+    int recovered_poly[4];
     poly_from_eval(recovered_poly, quotient_eval);
     print_poly("Scaled recovered", recovered_poly);
 
     // Reverse our earlier scaling to recover D(x).
-    unscale(recovered_poly, two);
+    unscale(recovered_poly, 2);
     print_poly("Recovered values", recovered_poly);
 
     return 0;
